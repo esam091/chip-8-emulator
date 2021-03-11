a148cdc572699447f60495c26043e8e6669a0ad1
Samuel Edwin <esam091@gmail.com>
2021-03-12 01:17:49 +0700

    read from arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::env;
+
+enum OpCodes {
+
+}
+
+fn bytes_to_opcodes(bytes: Vec<u8>) {
+
+}
+
+fn main() -> Result<(), String> {
+  let args: Vec<String> = env::args().collect();
+
+  let file_name = &args[1];
+
+  let bytes = fs::read(file_name)
+    .map_err(|err| format!("Read failed from {}", file_name))?;
+
+  println!("{:02x?}", bytes);
+
+  Ok(())
 }
