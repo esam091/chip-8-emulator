74cb534f3c067251d16bafd3deb9eb3df99cdd13
Samuel Edwin <esam091@gmail.com>
2021-03-12 01:26:39 +0700

    use u16 for better decoding

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ enum OpCodes {
 
 }
 
-fn bytes_to_opcodes(bytes: Vec<u8>) -> Vec<OpCodes> {
+fn bytes_to_opcodes(bytes: Vec<u16>) -> Vec<OpCodes> {
   let mut opcodes = Vec::new();
 
   opcodes
@@ -16,10 +16,16 @@ fn main() -> Result<(), String> {
 
   let file_name = &args[1];
 
+  // Todo
   let bytes = fs::read(file_name)
     .map_err(|err| format!("Read failed from {}", file_name))?;
 
-  println!("{:02x?}", bytes);
+  // TODO: handle odd number of bytes
+  let u16_vec: Vec<u16> = bytes.chunks_exact(2)
+    .map(|a| u16::from_ne_bytes([a[0], a[1]]))
+    .collect();
+
+  println!("{:04x?}", u16_vec);
 
   Ok(())
 }
