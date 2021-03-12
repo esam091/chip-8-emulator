89714758a95945a7ebac3447386c680758312861
Samuel Edwin <esam091@gmail.com>
2021-03-12 19:56:59 +0700

    refactor code

--- src/program.rs
+++ src/program.rs
@@ -43,6 +43,98 @@ impl Machine {
         })
     }
 
+    fn handle_instruction(&mut self, instruction: Instruction) -> Option<UIAction> {
+        match instruction {
+            Instruction::ClearScreen => {
+                self.pixel_buffer = [[false; 64]; 32];
+
+                return Some(UIAction::ClearScreen);
+            }
+
+            Instruction::StoreAddrToI(addr) => {
+                self.i = addr;
+
+                return None;
+            }
+
+            Instruction::SetV { register, value } => {
+                self.registers[register as usize] = value;
+
+                return None;
+            }
+
+            Instruction::Draw {
+                register_x,
+                register_y,
+                bytes,
+            } => {
+                let x = self.registers[register_x as usize] % (NUM_COLS as u8);
+                let mut y = self.registers[register_y as usize] % (NUM_ROWS as u8);
+
+                // println!("start drawing at {}, {}", x, y);
+
+                // try using self.vf to simplify the code
+                self.registers[0xf] = 0;
+
+                for index in 0..bytes {
+                    if y >= 32 {
+                        break;
+                    }
+
+                    let location = self.i as usize + index as usize;
+                    let sprite_bytes = self.memory[location];
+
+                    // println!(
+                    //     "extracting sprite at {:02x?}, value: {:#08b}",
+                    //     location, sprite_bytes
+                    // );
+
+                    let mut current_x = x;
+                    for col in 0..8 {
+                        if current_x >= 64 {
+                            break;
+                        }
+
+                        let address = &mut self.pixel_buffer[y as usize][current_x as usize];
+                        let pixel_value = *address as u8;
+
+                        let sprite_value = sprite_bytes & (1 << (7 - col));
+
+                        if sprite_value != 0 {
+                            if pixel_value == 1 {
+                                *address = false;
+                                self.registers[0xf] = 1;
+                            } else {
+                                // println!("turn on at {}, {}", current_x, y);
+                                *address = true;
+                            }
+                        }
+
+                        current_x += 1;
+                    }
+
+                    y += 1
+                }
+
+
+                return Some(UIAction::Draw(&self.pixel_buffer));
+            }
+
+            Instruction::AddToRegister { register, value } => {
+                self.registers[register as usize] += value;
+                return None;
+            }
+
+            Instruction::JumpToAddress(address) => {
+                self.program_counter = address as usize;
+                return None;
+            }
+
+            _ => return None,
+        } 
+        
+    }
+
     pub fn step(&mut self) -> Option<UIAction> {
         let a = self.memory[self.program_counter];
         let b = self.memory[self.program_counter + 1];
@@ -53,96 +145,10 @@ impl Machine {
         self.program_counter += 2;
         println!("instruction: {:#04x?}, opcode {:02x?}", opcode, instruction);
 
-        if let Some(opcode) = instruction {
-            match opcode {
-                Instruction::ClearScreen => {
-                    self.pixel_buffer = [[false; 64]; 32];
-
-                    return Some(UIAction::ClearScreen);
-                }
-
-                Instruction::StoreAddrToI(addr) => {
-                    self.i = addr;
-
-                    return None;
-                }
-
-                Instruction::SetV { register, value } => {
-                    self.registers[register as usize] = value;
-                }
-
-                Instruction::Draw {
-                    register_x,
-                    register_y,
-                    bytes,
-                } => {
-                    let x = self.registers[register_x as usize] % (NUM_COLS as u8);
-                    let mut y = self.registers[register_y as usize] % (NUM_ROWS as u8);
-
-                    // println!("start drawing at {}, {}", x, y);
-
-                    // try using self.vf to simplify the code
-                    self.registers[0xf] = 0;
-
-                    for index in 0..bytes {
-                        if y >= 32 {
-                            break;
-                        }
-
-                        let location = self.i as usize + index as usize;
-                        let sprite_bytes = self.memory[location];
-
-                        // println!(
-                        //     "extracting sprite at {:02x?}, value: {:#08b}",
-                        //     location, sprite_bytes
-                        // );
-
-                        let mut current_x = x;
-                        for col in 0..8 {
-                            if current_x >= 64 {
-                                break;
-                            }
-
-                            let address = &mut self.pixel_buffer[y as usize][current_x as usize];
-                            let pixel_value = *address as u8;
-
-                            let sprite_value = sprite_bytes & (1 << (7 - col));
-
-                            if sprite_value != 0 {
-                                if pixel_value == 1 {
-                                    *address = false;
-                                    self.registers[0xf] = 1;
-                                } else {
-                                    // println!("turn on at {}, {}", current_x, y);
-                                    *address = true;
-                                }
-                            }
-
-                            current_x += 1;
-                        }
-
-                        y += 1
-                    }
-
-
-                    return Some(UIAction::Draw(&self.pixel_buffer));
-                }
-
-                Instruction::AddToRegister { register, value } => {
-                    self.registers[register as usize] += value;
-                }
-
-                Instruction::JumpToAddress(address) => {
-                    self.program_counter = address as usize;
-                }
-
-                _ => return None,
-            } 
-        } else {
-            panic!("Failed to translate opcode: {:#02x?}, either the opcode is not supported yet, or there is a bug in the interpreter", opcode);
+        match instruction {
+            None => panic!("Failed to translate opcode: {:#02x?}, either the opcode is not supported yet, or there is a bug in the interpreter", opcode),
+            Some(instruction) => return self.handle_instruction(instruction)
         }
-
-        return None;
     }
 
 }
