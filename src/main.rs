3dc815892c9aee06601ffd695310f539800751ab
Samuel Edwin <esam091@gmail.com>
2021-03-12 14:39:46 +0700

    trying to draw

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, ops::Index};
+use std::{env, ops::Index, usize};
 use std::{convert::TryInto, fs, time::Duration};
 
 use sdl2::{event::Event, keyboard::Keycode, pixels::Color, rect::Rect};
@@ -14,7 +14,7 @@ enum OpCode {
     // 6XNN
     SetV {
         register: u8,
-        value: u16,
+        value: u8,
     },
 
     // DXYN
@@ -63,6 +63,10 @@ fn split_instruction(instruction: u16) -> (u8, u8, u8, u8) {
     );
 }
 
+fn combine_nibble2t(a: u8, b: u8) -> u8 {
+    (a << 4) ^ b
+}
+
 fn combine_nibble2(a: u8, b: u8) -> u16 {
     (a as u16) << 4 ^ (b as u16)
 }
@@ -78,7 +82,7 @@ fn instruction_to_opcode(instruction: u16) -> Option<OpCode> {
         (0xa, a, b, c) => Some(OpCode::StoreAddrToI(combine_nibble3(a, b, c))),
         (0x6, register, a, b) => Some(OpCode::SetV {
             register,
-            value: combine_nibble2(a, b),
+            value: combine_nibble2t(a, b),
         }),
         (0xd, register_x, register_y, bytes) => Some(OpCode::Draw {
             register_x,
@@ -120,9 +124,9 @@ struct Program {
     memory: [u8; 4096],
     program_counter: usize,
     
-    v0: u8,
-    v1: u8,
-    v2: u8,
+    registers: [u8; 16],
+    i: u16,
+    pixel_buffer: [[bool; 64]; 32]
 }
 
 impl Program {
@@ -139,10 +143,9 @@ impl Program {
         Ok(Program {
             memory,
             program_counter: 0,
-
-            v0: 0,
-            v1: 0,
-            v2: 0,
+            registers: [0; 16],
+            i: 0,
+            pixel_buffer: [[false; 64]; 32]
         })
     }
 
@@ -157,9 +160,70 @@ impl Program {
         if let Some(opcode) = opcode {
             match opcode {
                 OpCode::ClearScreen =>  {
+                    self.pixel_buffer = [[false; 64]; 32];
                     self.program_counter += 2;
+
                     return Some(UIAction::ClearScreen)
                 },
+
+                OpCode::StoreAddrToI(addr) => {
+                    self.program_counter += 2;
+                    self.i = addr;
+
+                    return None
+                },
+
+                OpCode::SetV {
+                    register,
+                    value
+                } => {
+                    self.registers[register as usize] = value;
+                    self.program_counter += 2;
+                },
+
+                OpCode::Draw { register_x, register_y, bytes } => {
+                    let x = self.registers[register_x as usize] % 64;
+                    let mut y = self.registers[register_y as usize] % 32;
+
+                    // try using self.vf to simplify the code
+                    self.registers[0xf] = 0;
+
+                    for index in 0..bytes {
+                        if y >= 32 {
+                            break;
+                        }
+
+                        let location = self.i as usize + index as usize;
+                        let sprite_bytes = self.memory[location];
+
+                        let mut current_x = x;
+                        for col in 0..8 {
+                            if current_x >= 64 {
+                                break;
+                            }
+
+                            let pixel_value = self.pixel_buffer[y as usize][current_x as usize] as u8;
+
+                            let sprite_value = sprite_bytes & (1 << col);
+                            
+                            if sprite_value == 1 {
+                                if pixel_value == 1 {
+                                    self.pixel_buffer[y as usize][current_x as usize] = false;
+                                    self.registers[0xf] = 1;
+                                } else {
+                                    self.pixel_buffer[y as usize][current_x as usize] = true;
+                                }
+                            }
+
+                            current_x += 1;
+                        }
+
+                        y += 1
+                    }
+
+                    self.program_counter += 2;
+                }
+
                 _ => return None
             }
         }
